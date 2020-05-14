3545b18d505354540fe7beadfb2226597a2f0b2c
Michael <michael.baertschi@zebbra.ch>
2020-05-14 16:08:23 +0200

    Configure vuetify and lint-staged

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -73,24 +73,12 @@ const config: Configuration = {
 
   /*
    ** vuetify module configuration
-   ** https://github.com/nuxt-community/vuetify-module
+   ** See https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
-    // theme: {
-    //   dark: true,
-    //   themes: {
-    //     dark: {
-    //       primary: colors.blue.darken2,
-    //       accent: colors.grey.darken3,
-    //       secondary: colors.amber.darken3,
-    //       info: colors.teal.lighten1,
-    //       warning: colors.amber.base,
-    //       error: colors.deepOrange.accent4,
-    //       success: colors.green.accent3
-    //     }
-    //   }
-    // }
+    treeShake: true,
+    optionsPath: "~/vuetify.options.ts",
   },
 
   /*
