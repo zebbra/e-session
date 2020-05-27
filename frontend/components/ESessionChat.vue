45dfbe5a239551881323efcd1b746a7ecd6f858b
Leandro Lerena <leandro@lerena.ch>
2020-05-27 15:16:59 +0200

    Chat checks now if joined room, ui

--- frontend/components/ESessionChat.vue
+++ frontend/components/ESessionChat.vue
@@ -1,6 +1,24 @@
 <template>
   <section>
-    <input v-model="text" placeholder="Enter a message" />
+    <v-card>
+      <v-card-title>Messages</v-card-title>
+        <v-card-text v-if="room">
+          <v-card-text>
+            {{ room.name }}
+          </v-card-text>
+          <v-list v-for="(message, index) in room.messages" :key="index">
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ message.author.name }} - {{ message.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list>
+        </v-card-text>
+        <v-card-text v-else>
+          No room joined
+        </v-card-text>
+    </v-card>
+    <input v-model="message" placeholder="Enter a message" />
     <button @click="addMessage()">
       Send message
     </button>
@@ -14,23 +32,35 @@ import { sessionStore, moderationStore } from "~/store";
 
 export default defineComponent({
   name: "ESessionChat",
-  props: {
-    room: String,
-  },
+
   setup() {
+    console.log("Setup ESessionChat")
     const user = computed(() => sessionStore.user);
     const room = computed(() => moderationStore.room);
     const message = ref("");
+    const chatActivated = computed(() => room.value != null && user.value != null)
     const { mutate: sendMessage } = useSendMessage(room, user, message);
 
     function addMessage() {
-      if (message.value !== "") {
-        sendMessage();
+      console.log(chatActivated.value)
+      console.log(user.value)
+      console.log(room.value)
+      if(chatActivated.value){
+        console.log("send message " + message.value + ".")
+        if (message.value !== "") {
+          sendMessage();
+        }
+      } else {
+        console.log("Join room first")
       }
+
     }
 
     return {
       addMessage,
+      user, 
+      room,
+      message
     };
   },
 });
