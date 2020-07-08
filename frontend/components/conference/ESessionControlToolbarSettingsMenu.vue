5c4e4a5d855eb0028777b0772d5ac679ceb8f54f
Marek Paulson <marek@paulson.ee>
2020-07-08 07:10:16 +0200

    toggle between fullscreen

--- frontend/components/conference/ESessionControlToolbarSettingsMenu.vue
+++ frontend/components/conference/ESessionControlToolbarSettingsMenu.vue
@@ -8,7 +8,12 @@
       </template>
       <!-- <v-list-item-title>{{ item.title }}</v-list-item-title> -->
       <v-list dense>
-        <v-list-item v-for="(item, i) in items" :key="i" @click="menuSelection">
+        <v-list-item
+          v-for="(item, i) in items"
+          :id="item.id"
+          :key="i"
+          @click="menuSelection"
+        >
           <v-list-item-icon>
             <v-icon v-text="item.icon"></v-icon>
           </v-list-item-icon>
@@ -26,21 +31,54 @@
 <script lang="ts">
 import { defineComponent, ref } from "nuxt-composition-api";
 import consola from "consola";
+import { openFullscreen, closeFullscreen } from "~/utils/helpers";
 
 export default defineComponent({
   name: "ESessionControlToolbarSettingsMenu",
 
   setup() {
     const items = ref([
-      { text: "Settings", icon: "mdi-cog" },
-      { text: "Fullscreen", icon: "mdi-fullscreen" },
-      { text: "Hide Background", icon: "mdi-blur" },
-      { text: "Toggle Grid", icon: "mdi-grid" },
+      { text: "Settings", icon: "mdi-cog", id: "SHOW_SETTINGS" },
+      { text: "Fullscreen", icon: "mdi-fullscreen", id: "TOGGLE_FULLSCREEN" },
+      /* { text: "Hide Background", icon: "mdi-blur" }, */
+      { text: "Toggle Grid", icon: "mdi-grid", id: "TOGGLE_GRID" },
+      /* { text: "Record Session", icon: "mdi-record" }, */
     ]);
+    const appIsFullscreen = ref(false);
 
-    function menuSelection() {
-      consola.log("select");
+    function menuSelection(event: any) {
+      switch (event.target.id) {
+        case "SHOW_SETTINGS":
+          consola.log("SHOW_SETTINGS");
+          break;
+        case "TOGGLE_FULLSCREEN":
+          if (appIsFullscreen.value) {
+            closeFullscreen(document);
+          } else {
+            openFullscreen(document.body);
+            document.body.onfullscreenchange = handleFullscreenChange;
+          }
+          break;
+        case "TOGGLE_GRID":
+          consola.log("TOGGLE_GRID");
+          break;
+        default:
+          consola.log("DEFAULT");
+      }
     }
+
+    function handleFullscreenChange(event) {
+      const elem = event.target;
+      const isFullscreen = document.fullscreenElement === elem;
+      if (isFullscreen) {
+        // consola.log("Full start");
+        appIsFullscreen.value = true;
+      } else {
+        // consola.log("Full end");
+        appIsFullscreen.value = false;
+      }
+    }
+
     return {
       menuSelection,
       items,
--- frontend/utils/helpers.ts
+++ frontend/utils/helpers.ts
@@ -12,4 +12,35 @@ function delay(ms: number = 1000) {
   return p;
 }
 
-export { delay };
+/* View in fullscreen */
+function openFullscreen(elem: any) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen(document) {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
+export { delay, openFullscreen, closeFullscreen };
