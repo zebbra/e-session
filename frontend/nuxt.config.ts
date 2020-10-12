9514bde3bdeb0d89f90e1ba9fa253654eca892da
Marek Paulson <marek@paulson.ee>
2020-10-12 11:43:26 +0200

    right jitsi lib

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -20,7 +20,7 @@ const config: Configuration = {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     script: [
-      { src: "https://meet.jit.si/libs/lib-jitsi-meet.min.js" },
+      { src: "https://hello.zebbra.ch/libs/lib-jitsi-meet.min.js" },
       {
         src:
           "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js",
