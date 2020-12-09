eb510e301f007c7f1b319ed2fd87839df6b40051
Marek Paulson <marek@paulson.ee>
2020-12-09 23:22:25 +0100

    new tagline

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -8,14 +8,14 @@ const config: Configuration = {
    */
   head: {
     titleTemplate: "%s - E-Session",
-    title: "Join Room",
+    title: "Join ",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || "",
+        content: "The Online Parliament Showcase",
       },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
